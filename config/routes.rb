de36122168746cb97f9875c58bdfb928f2881714
Lauren Perchitti <laurenperchitti@gmail.com>
2018-05-30 20:16:00 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :students, only: :show
-  get 'students/:id', to: 'students#show'
+  #get 'students/:id', to: 'students#show'
   resources :students, only: :index
 end
